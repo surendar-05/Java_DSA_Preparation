2f7ac52453e8356e9b9f36453603075516c53aea
surendar-05 <surendarrockzz7@gmail.com>
2025-03-02 23:01:40 +0530

    commit

---
+++ dynamicprogramming/climbingstairs.java
@@ -0,0 +1,17 @@
+package dynamicprogramming;
+
+class Solution {
+    public int climbStairs(int n) {
+       int prev=1;
+       int prev2=1;
+
+       for(int i=2;i<=n;i++){
+        int sum=prev+prev2;
+          prev2=prev;
+          prev=sum;
+        
+       }
+       return prev;
+
+    }
+}
